3ffdc56d1f391375960580a833fe5b7dd07c0821
Oleksandr Horbenko <oleksandr.horbenko.01@gmail.com>
2024-12-23 14:44:42 +0200

    SV

--- index.js
+++ index.js
@@ -11,10 +11,12 @@ import { showTableResults } from "./showTableResults.js";
 import { fetchApiGetCountries } from "./fetchApiGetCountries.js";
 import { handlerEvents } from "./handlerEvents.js";
 import { fetchApiWeatherCity } from "./fetchApiWeatherCity.js";
+// import { getFilterResult } from "./getFilterResult.js";
 
 let allCountries = [];
-let visitedCountries;
 let selectCountryName = "";
+let visitedCountries;
+let citiesSelectedCountry;
 
 document.addEventListener("DOMContentLoaded", async() => {
     const formWeather = document.querySelector("#form-weather");
@@ -29,42 +31,8 @@ document.addEventListener("DOMContentLoaded", async() => {
 
     
     allCountries = await fetchApiGetCountries(allCountries, selectCountry, selectCity);
+    elemInputCityName.disabled = true;
 
-    // elemInputCountryName.addEventListener("input", (event) => {
-    //     const inputCountryName = event.target.value.trim().toLowerCase();
-    //     selectCountry.innerHTML = "";
-    //     selectCountry.append(defaultOptionCountry);
-    //     conditionWeatherCity.innerHTML = "";
-
-    //     if (inputCountryName === "") {
-    //         showListCountries(allCountries, selectCountry);
-    //         selectCity.innerHTML = "";
-    //         selectCity.append(defaultOptionCity);
-    //         selectCity.disabled = true;
-    //         selectCountry.disabled = false;
-    //     } else {
-    //         const filterCountries = allCountries.filter((country) => 
-    //             country.name.common.toLowerCase().startsWith(inputCountryName)
-    //         );
-            
-    //         if (filterCountries.length > 0) {
-    //             filterCountries.forEach((country) => {
-    //                 const option = document.createElement("option");
-    //                 option.value = country.name.common;
-    //                 option.textContent = country.name.common;
-    //                 selectCountry.append(option);
-    //             });
-    //         } else {
-    //             formWeather.append(conditionWeatherCity)
-    //             selectCountry.disabled = true;
-    //             conditionWeatherCity.innerHTML = "<p>Такої назви країни не існує.</p>"
-    //         }
-            
-    //         selectCity.innerHTML = "";
-    //         selectCity.append(defaultOptionCity);
-    //         selectCity.disabled = true;
-    //     };
-    // });
     handlerEvents(elemInputCountryName, "input", (event) => {
         const inputCountryName = event.target.value.trim().toLowerCase();
         selectCountry.innerHTML = "";
@@ -94,6 +62,8 @@ document.addEventListener("DOMContentLoaded", async() => {
                 selectCountry.disabled = true;
                 conditionWeatherCity.innerHTML = "<p>Такої назви країни не існує.</p>"
             }
+            // getFilterResult(allCountries, inputCountryName, selectCountry, formWeather, conditionWeatherCity, true);
+            // selectCountry.disabled = true;
             
             selectCity.innerHTML = "";
             selectCity.append(defaultOptionCity);
@@ -101,39 +71,9 @@ document.addEventListener("DOMContentLoaded", async() => {
         };
     });
 
-    // selectCountry.addEventListener("change", async (event) => {
-    //     selectCountryName = event.target.value;
-    //     conditionWeatherCity.innerHTML = "";
-
-    //     const foundCountry = allCountries.find(country => country.name.common === selectCountryName );
-    //     saveVisitedCountries(foundCountry, visitedCountries);
-    //     showTableResults(tableInformationOfCountries, foundCountry);
-
-    //     if (selectCountryName) {
-    //         try {
-    //             const citiesSelectedCountry = await fetchApiCitiesByCountry(selectCountryName);
-
-    //             const foundCountry = allCountries.find(country => country.name.common === selectCountryName);
-    //             let countryCode = "en";
-
-    //             if (foundCountry && foundCountry.languages) {
-    //                 const languagesKeys = Object.keys(foundCountry.languages);
-                    
-    //                 if (languagesKeys.length > 0) {
-    //                     const firstLanguageCode = languagesKeys[0];
-    //                     countryCode = firstLanguageCode.slice(0,2);
-    //                 };
-    //             };
-
-    //             showListCities(citiesSelectedCountry, selectCity, defaultOptionCity, countryCode);
-    //             selectCity.disabled = false;
-    //         } catch (error) {
-    //             console.error("Помилка при завантаженні міст:", error);
-    //         }
-    //     }
-    // });
     handlerEvents(selectCountry, "change", async(event) => {
         selectCountryName = event.target.value;
+        elemInputCityName.disabled = false;
         conditionWeatherCity.innerHTML = "";
 
         const foundCountry = allCountries.find(country => country.name.common === selectCountryName );
@@ -142,7 +82,7 @@ document.addEventListener("DOMContentLoaded", async() => {
 
         if (selectCountryName) {
             try {
-                const citiesSelectedCountry = await fetchApiCitiesByCountry(selectCountryName);
+                citiesSelectedCountry = await fetchApiCitiesByCountry(selectCountryName);
 
                 const foundCountry = allCountries.find(country => country.name.common === selectCountryName);
                 let countryCode = "en";
@@ -164,36 +104,34 @@ document.addEventListener("DOMContentLoaded", async() => {
         };
     });
 
-    elemInputCityName.addEventListener("input", (event) => {
+    handlerEvents(elemInputCityName, "input", (event) => {
         const inputCityName = event.target.value.trim().toLowerCase();
         console.log(inputCityName);
+        
+        console.log(citiesSelectedCountry);
+
+        selectCity.innerHTML = "";
+        selectCity.append(defaultOptionCity);
+
+
+        // getFilterResult(citiesSelectedCountry, inputCityName, selectCity, formWeather, conditionWeatherCity, false);
+        const filterCities = citiesSelectedCountry.filter((city) => {
+            return city.toLowerCase().startsWith(inputCityName);
+        });
+        console.log(filterCities);
+
+        if (filterCities.length > 0) {
+            filterCities.forEach((city) => {
+                const option = document.createElement("option");
+                option.value = city;
+                option.textContent = city;
+                selectCity.append(option);
+            });
+        } else {
+            formWeather.append(conditionWeatherCity)
+            conditionWeatherCity.innerHTML = "<p>Такої назви країни не існує.</p>"
+        }
     });
 
     fetchApiWeatherCity(selectCity, selectCountryName, conditionWeatherCity, formWeather);
-    // handlerEvents(selectCity, "change", async(event) => {
-    //     const selectCityName = event.target.value;
-    //     if (selectCityName) {
-    //         try {
-    //             const response = await fetch(`${API_BASE_URL}/current.json?key=${API_KEY}&q=${selectCityName}&q=${selectCountryName}`);
-    //             const dataWeatherCity = await response.json();
-    //             console.log(dataWeatherCity);
-    //             getDataWeatherCity(dataWeatherCity, conditionWeatherCity, formWeather);
-    //         } catch (error) {
-    //             console.error("Сталася помилка:", error);
-    //         };
-    //     };
-    // });
-    // selectCity.addEventListener("change", async(event) => {
-    //     const selectCityName = event.target.value;
-    //     if (selectCityName) {
-    //         try {
-    //             const response = await fetch(`${API_BASE_URL}/current.json?key=${API_KEY}&q=${selectCityName}&q=${selectCountryName}`);
-    //             const dataWeatherCity = await response.json();
-    //             console.log(dataWeatherCity);
-    //             getDataWeatherCity(dataWeatherCity, conditionWeatherCity, formWeather);
-    //         } catch (error) {
-    //             console.error("Сталася помилка:", error);
-    //         };
-    //     };
-    // });
 });
